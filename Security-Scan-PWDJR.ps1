6560e422c84eb0de9f788fffc56f39f96db4c6e1
JulienRopert <98157356+JulienRopert@users.noreply.github.com>
2023-03-22 10:47:53 +0100

    Add files via upload

---
+++ Security-Scan-PWDJR.ps1
@@ -0,0 +1,218 @@
+﻿function createAmenu
+{
+    param([array]$listelements,[string]$title)
+         cls
+         [int]$counter = 0
+         write-host "- - - - - -",$title, "- - - - - -" -foregroundcolor Cyan
+         oneemptyline
+         forEach($element in $listelements)
+         {
+             $counter++
+             write-host "`t",$counter,". ",$element -Separator '' -ForegroundColor white
+         }
+         oneemptyline   
+         return makeachoice
+}
+function oneemptyline
+{
+   Write-Host "`n"
+}
+function makeachoice
+{
+    Write-Host "Faites votre choix" -NoNewline -ForegroundColor Cyan
+    try
+    {
+        [int]$theIndex = Read-Host
+    }
+    Catch
+    {}
+    return $theIndex
+    }
+function mainMenu
+{
+     [array]$theMainMenu = "README",`
+                           "Detecteur de vulnérabilité",`
+                           "OSINT",`
+                           "Quitter"
+     [string]$TheMenuTitle = "Menu Principal"
+     do
+     {
+         [int]$theChosenIndex = createAmenu $theMainMenu $TheMenuTitle
+     }
+     while($theChosenIndex -lt 1 -or $theChosenIndex -gt $theMainMenu.count -or !$theChosenIndex)
+     cls
+     if($theChosenIndex -eq 1)
+     {
+         submenuOne
+     }
+     elseif($theChosenIndex -eq 2)
+     {
+         submenuTwo
+     }
+     elseif($theChosenIndex -eq 3) 
+     {
+         submenuThree
+     }
+     else
+     {
+         submenuFour
+     }    
+}
+function submenuOne
+{
+    [array]$firstSubmenu = "Afficher les conditions d'utilisation",`
+                           "Installation des Dépendances",`
+                           "Revenir au menu principal"
+    [string]$theMenuTitle = "Readme"
+    Do
+    {
+        [int]$theChosenIndex = createAmenu $firstSubmenu $theMenuTitle
+    }
+    while($theChosenIndex -lt 1 -or $theChosenIndex -gt $firstSubmenu.count -or !$theChosenIndex)
+    cls
+    if($theChosenIndex -eq 1)
+    {
+     
+      Write-Host " "
+      Write-Host " "
+      Write-Host "    Le toolbox a pour seul objectif d'ameliorer la securitée des systemes d'informations " -ForegroundColor red
+      write-host "    et non a exploiter les informations récuperées." -ForegroundColor red
+      Write-Host " "
+      Write-Host " "
+      Write-Host " "
+      Write-Host "                      **                                                 **                     "
+      Write-Host "                     ****                                               ****                    "
+      Write-Host "                    ******                                             ******                   "
+      Write-Host "                   ********                                           ********                  "
+      Write-Host "                  *** || ***                                         *** || ***                 "
+      Write-Host "                 ***  ||  ***                                       ***  ||  ***                "
+      Write-Host "                ***   ||   ***                                     ***   ||   ***               "
+      Write-Host "               ***    ||    ***                                   ***    ||    ***              "
+      Write-Host "              ***     ||     ***                                 ***     ||     ***             "
+      Write-Host "             **                **                               **                **            "
+      Write-Host "            **        OO        **                             **        OO        **           "
+      Write-Host "           ************************                           ************************          "
+      Write-Host "          **************************                         **************************         "
+      Write-Host "                                                                          "
+      Write-Host " "
+      write-host "    L’article 323-1 du code pénal sanctionne«le fait d’accéder ou de se maintenir frauduleusement "
+      write-host "    dans tout ou partie d’un système de traitement automatisé». La peine encourue est 2 ans d’emprisonnement "
+      write-host "    et 30000€ d’amende. Celle-ci peut être portée à 3 ans d’emprisonnement et 45000€ d’amende lorsqu’il en résulte "
+      write-host "    «soit la suppression, soit la modifications de données contenues dans le système, soit un altération du fonctionnement de ce système»."
+      Write-Host " "
+      Write-Host "                                                                          "
+      Write-Host " "
+      Write-Host " "
+      pause
+      mainMenu   
+    }
+    ElseIf($theChosenIndex -eq 2)
+    {
+   bash -c sudo su
+   bash -c apt update
+   bash -c apt upgrade
+   bash -c pip install -r requirements.txt
+   write-host "Les dépendances ont été installées."
+
+
+         pause
+      mainMenu  
+
+    }
+Else
+{
+mainMenu
+}
+}
+function submenuTwo
+{
+    [array]$firstSubmenu = "Webapp",`
+                           "Interne/OPENVAS",`
+                       "Revenir au menu principal"
+    [string]$theMenuTitle = "Detecteur de vulnérabilité"
+    Do
+    {
+        [int]$theChosenIndex = createAmenu $firstSubmenu $theMenuTitle
+    }
+    while($theChosenIndex -lt 1 -or $theChosenIndex -gt $firstSubmenu.count -or !$theChosenIndex)
+    cls
+       if($theChosenIndex -eq 1)
+    {
+#Execution de l'option "Application WEB"
+Write-Host "Entrez l'IP ou le nom d'hôte de la cible  :"
+
+#Ajout d'options pour l'execution de NMAP
+$target = Read-Host
+Write-Host "Ajoutez les options pour NMAP :"
+$options = Read-Host
+
+#Ajout d'options pour l'execution de NIKTO
+Write-Host "Ajoutez les options pour nikto :"
+$optionsnikto = Read-host
+
+#Execution de NMAP et des options s'il y en a
+Write-Host "Exécution du scan NMAP en cours..."
+start-process nmap "$target $options -oN nmap_$target.txt" -NoNewWindow -PassThru -wait
+Write-Host "Scan NMAP terminé, les résultats sont enregistrés dans le fichier 'nmap_$target.txt'"
+
+#Execution de NIKTO et des options s'il y en a
+Write-Host "Exécution du scan Nikto en cours..."
+Start-process nikto "-h $target $optionsnikto -o nikto_$target.txt" -NoNewWindow -PassThru -wait
+Write-Host "Scan Nikto terminé, les résultats sont enregistrés dans le fichier 'nikto_$target.txt'"
+      pause
+      mainMenu  
+
+    }
+    ElseIf($theChosenIndex -eq 2)
+    {
+    bash -c GVM/gvm-scan.sh > Scan_OPENVAS_ID.txt
+    Write-Host "le scan est en court, vous pouvez consulter l'avancer sur la console web https://127.0.0.1:9392/login"
+    Write-host "Vous pouvez exporter le resultat en pdf en executant ./pdf_openvas.sh, vous aurez besoin de l'id generer dans le document Scan_OPENVAS_ID.txt"
+    pause
+      mainMenu 
+	}
+
+    
+Else
+{
+mainMenu
+}
+}
+function submenuThree
+{
+[array]$thridSubmenu = 
+                       "WebScapping ",`
+                       "Google DORKS",`
+                       "Revenir au menu principal"
+[string]$theMenuTitle = "OSINT"
+Do
+{
+[int]$theChosenIndex = createAmenu $thridSubmenu $theMenuTitle
+}
+while($theChosenIndex -lt 1 -or $theChosenIndex -gt $thridSubmenu.count -or !$theChosenIndex)
+cls
+if($theChosenIndex -eq 1)
+{
+ python3 Dolkings/dolkings.py 
+ pause
+      mainMenu
+}
+    ElseIf($theChosenIndex -eq 2)
+      { 
+      python3 Web-Scraper/web_scraper.py 
+      pause
+      mainMenu
+      
+      }
+   
+       
+Else
+{
+mainMenu
+}
+}
+function submenuFour
+{
+     Write-Host "--------------Fermeture du script--------------------" -ForegroundColor Red
+}
+mainmenu
---
+++ Web-Scraper/README.md
@@ -0,0 +1,95 @@
+# Web Scraper
+##### _A Melange of Web Tools for Web Hacking_
+<p align="center">
+  <img  width="250" src="Images/Logo.png" />
+</p>
+
+<p align="center">
+<img src=https://img.shields.io/badge/Made%20with-Python-blue>
+<img src=https://img.shields.io/badge/Python-3.7-green>
+<img src=https://img.shields.io/badge/Version-1.0-yellowgreen>
+<img src=https://img.shields.io/badge/OS-Linux-yellow>
+</p>
+<p align="center">
+    <h3 align="center"> Web Scraper is a Python based Tool for web hacking and assesment. Web Scraper is a cluster of 20 most used tools to execute attacks from single click </h3>
+</p>
+
+***
+#### DISCLAIMER :
+
+The Developer of Web Scraper will not be held responsible if the tool is used with malicious or criminal intent. Use at your own risk !
+
+*** 
+#### USES OF WEB SCRAPER :
+- This tool could be used for web hacking
+- You can also perform Reconassiance using this tool
+- Can be helpful for Bug Bounty 
+- Can be used for scraping sensitive information
+
+***
+#### FEATURES :
+
+ 1. Find ASN
+ 2. HTTP Header
+ 3. Find Subdomain
+ 4. Find Web Technology
+ 5. Find Admin Panel
+ 6. Find Directories
+ 7. Whois Information
+ 8. Port Scan
+ 9. TCP Scan
+ 10. UDP Scan
+ 11. External Links
+ 12. Banner Grab
+ 13. Subnet Lookup
+ 14. Reverse IP Lookup
+ 15. Geo-Location
+ 16. DNS Lookup
+ 17. Trace Route
+ 18. Firewall Detect
+ 19. Vulnerability Scan
+ 20. Zone Transfer  
+
+***
+#### INSTALLATION :
+Web Scraper requires Python 3.7 or higher version to run.
+```
+$ git clone https://github.com/Encryptor-Sec/Web-Scraper.git
+```
+#### REQUIREMENTS :
+Please install the required libraries in order to run the tool
+```
+$ pip install -r requirements.txt
+```
+
+#### USAGE :
+The tool can be executed by running the below commannd
+```
+$ python3 web_scraper.py
+```
+
+
+<p align="center">
+<img src=https://github.com/Encryptor-Sec/Web-Scraper/blob/main/Images/Tool_image.PNG>
+</p>
+
+The tool will ask to Enter a target for assesment. You can either put IP address of the target or the Domain name
+```
+Enter Target Address [Example.com / IP] : google.com
+```
+ Then the tool will check if the target phyically exists or not and it will show the target status [Live / Down]
+ 
+ Then it will prompt you to choose a particular option for assesing the target
+
+
+***
+#### LICENSE
+[MIT-License](LICENSE)
+***
+#### More suggestions and contributions are highly appreciated to make this tool better.
+### _STAY SAFE, ACT SMART_
+### Hit Me Up
+[![Twitter ](https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/_encryptor_)
+[![Instagram](https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/xhackerboyy)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/sathyaprakashsahoo)
+[![Website](https://img.shields.io/badge/Website-FF5722?style=for-the-badge&logo=blogger&logoColor=white)](https://www.cyberbuddy.co.in)
