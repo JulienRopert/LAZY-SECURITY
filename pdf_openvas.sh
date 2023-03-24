eb2192e0b01f841101ad0237ce357edb3e95cf95
JulienRopert <98157356+JulienRopert@users.noreply.github.com>
2023-03-25 00:40:30 +0100

    Update pdf_openvas.sh

--- pdf_openvas.sh
+++ pdf_openvas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+## Récupere les information d'identifications
 read -p "Entrez l'username: " username
 read -p "Entrez le password: " password
 read -p "Entrez l'id généré précedemment: " ID
-
+##Execute la commande gvm-script avec un script "export-pdf" pour GMP qui va exporter en pdf le résultat.
 gvm-script --gmp-username $username --gmp-password $password socket GVM/export-pdf-report.gmp.py $ID $ID.txt
