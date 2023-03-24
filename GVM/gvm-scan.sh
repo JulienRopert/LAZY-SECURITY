3706e468b6edeaa24da87b8d8721acb0000583f1
JulienRopert <98157356+JulienRopert@users.noreply.github.com>
2023-03-24 23:43:07 +0100

    Delete GVM directory

--- GVM/gvm-scan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Enter the username: " username
-read -p "Enter the password: " password
-read -p "Enter the IP address: " ip_address
-
-gvm-script --gmp-username $username --gmp-password $password socket GVM/scan-new-system.gmp.py $ip_address 4a4717fe-57d2-11e1-9a26-406186ea4fc5
--- GVM/pdf_openvas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Entrez l'username: " username
-read -p "Entrez le password: " password
-read -p "Entrez l'id généré précedemment: " ID
-
-gvm-script --gmp-username $username --gmp-password $password socket GVM/export-pdf-report.gmp.py $ID $ID.txt
