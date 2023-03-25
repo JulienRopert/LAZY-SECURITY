1301a34eb4a2686ad84860a7552f129013d40f3e
JulienRopert <98157356+JulienRopert@users.noreply.github.com>
2023-03-25 23:35:51 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -41,31 +41,3 @@ sudo cp /lib64/libopenvas_nasl.so.21.4.4 /lib
 # Ajout des autorisations d'execution des scripts.
 sudo chmod 777 gvm-scan.sh
 sudo chmod 777 Ovas.SH
-
-# CORRECTIF OPENVAS, gvm-script ne pouvant pas fonctionner en utilisateur root, il faut fournir l'acces a ses différents packages 
-# A l'utilisateur principal, y compris gvmd.sock qui est l'interface cli de Openvas.
-
-sudo chmod 777 /usr/lib/python3/dist-packages/gvm
-sudo chmod 777 /usr/lib/python3/dist-packages/gvmtools/script.py
-sudo chmod 777 /usr/lib/python3/dist-packages/gvmtools/helper.py
-sudo chmod 777 /usr/lib/python3/dist-packages/gvmtools
-sudo chmod 777 /var/run/gvmd/gvmd.sock
-sudo chmod 777 /etc/gvm/ospd-openvas.conf
-sudo chmod 777 /etc/gvm/gvmd_log.conf
-sudo chmod 777 /etc/gvm/gsad_log.conf
-sudo chmod 777 /etc/gvm/pwpolicy.conf
-sudo chmod 777 /usr/bin/ospd-openvas
-sudo chmod 777 /etc/openvas/openvas.conf
-sudo chmod 777 /etc/openvas/openvas_log.conf
-sudo chmod 777 /etc/openvas/gnupg
-sudo chmod 127 /var/log/gvm/openvas.log
-sudo chmod 777 run.sh
-
-
-
-# Changement du mot de passe, check de l'installation, demarrage de GVM, et mise a jour de la BDD CVE
-
-sudo gvm-feed-update
-sudo gvm-check-setup
-sudo gvm-start
-sudo -E -u _gvm -g _gvm gvmd --user=admin --new-password=admin
