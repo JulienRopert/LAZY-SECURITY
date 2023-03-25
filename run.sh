4f6e112ba9bb0120022805d38f66780420424493
JulienRopert <98157356+JulienRopert@users.noreply.github.com>
2023-03-25 21:30:09 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Rectification des droits, check de l'installation, et changement du mdp par défaut
+
 sudo chmod 127 /var/log/gvm/openvas.log
 sudo gvm-check-setup
 sudo gvm-start
+sudo -E -u _gvm -g _gvm gvmd --user=admin --new-password=admin
