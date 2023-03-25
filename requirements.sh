596523a140969a19b1f6788e423e7a4b66d410eb
JulienRopert <98157356+JulienRopert@users.noreply.github.com>
2023-03-25 23:10:11 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -46,6 +46,8 @@ sudo chmod 777 Ovas.SH
 # A l'utilisateur principal, y compris gvmd.sock qui est l'interface cli de Openvas.
 
 sudo chmod 777 /usr/lib/python3/dist-packages/gvm
+sudo chmod 777 /usr/lib/python3/dist-packages/gvmtools/script.py
+sudo chmod 777 /usr/lib/python3/dist-packages/gvmtools/helper.py
 sudo chmod 777 /usr/lib/python3/dist-packages/gvmtools
 sudo chmod 777 /var/run/gvmd/gvmd.sock
 sudo chmod 777 /etc/gvm/ospd-openvas.conf
